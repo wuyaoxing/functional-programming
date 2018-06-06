ae6f6f6c83e958b436a63c3b30d21f308499565a
wuyaoxing <82565097@qq.com>
2018-06-06 09:36:35 +0800

    map

--- src/functional.js
+++ src/functional.js
@@ -1,9 +1,17 @@
+// forEach
+// const forEach = (arr, fn) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     fn(arr[i]);
+//   }
+// };
+
 const forEach = (arr, fn) => {
-  for (let i = 0; i < arr.length; i++) {
-    fn(arr[i]);
+  for (const value of arr) {
+    fn(value);
   }
 };
 
+// forEachObject
 const forEachObject = (obj, fn) => {
   for (let property in obj) {
     if (obj.hasOwnProperty(property)) {
@@ -12,4 +20,13 @@ const forEachObject = (obj, fn) => {
   }
 };
 
-export { forEach, forEachObject };
+// map
+const map = (arr, fn) => {
+  const result = [];
+  for (const value of arr) {
+    result.push(fn(value));
+  }
+  return result;
+};
+
+export { forEach, forEachObject, map };
--- src/run.js
+++ src/run.js
@@ -1,4 +1,4 @@
-import { forEach, forEachObject } from "./functional";
+import { forEach, forEachObject, map } from "./functional";
 
 const arr = [1, 2, 3, 4, 5];
 const obj = { id: 1, name: "jon", age: 20 };
@@ -8,3 +8,6 @@ forEach(arr, item => console.log("forEach:", item, item * item));
 forEachObject(obj, (key, value) =>
   console.log(`forEachObject: ${key} is ${value}`)
 );
+
+const arrMap = map(arr, item => item * item)
+console.log('map:', arr, 'to', arrMap)
